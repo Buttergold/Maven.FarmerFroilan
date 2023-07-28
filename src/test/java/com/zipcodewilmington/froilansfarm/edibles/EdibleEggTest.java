0f61f867555b334dbfa0afa9ca2e0997d8e39075
Liz Kinker <Liz.Kinker@gmail.com>
2023-07-28 17:14:33 -0400

    Edible Egg Tests

--- src/test/java/com/zipcodewilmington/froilansfarm/edibles/EdibleEggTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/edibles/EdibleEggTest.java
@@ -1,4 +1,31 @@
 package com.zipcodewilmington.froilansfarm.edibles;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class EdibleEggTest {
+
+    @Test
+    public void testConstructor() {
+        EdibleEgg egg = new EdibleEgg();
+        Assert.assertNotNull(egg);
+    }
+
+    @Test
+    public void testInheritance(){
+        EdibleEgg egg = new EdibleEgg();
+        Assert.assertTrue(egg instanceof Edible);
+    }
+
+    @Test
+    public void testGetAndSetEaten(){
+        boolean expected = true;
+        EdibleEgg egg = new EdibleEgg();
+
+        egg.setHasBeenEaten = expected;
+        boolean actual = egg.getHasBeenEaten();
+
+        Assert.assertEquals(expected, actual);
+
+    }
 }
