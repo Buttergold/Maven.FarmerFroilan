8845e32e1d5c1537725e4317969fb5e7e019b9be
olamide <oadams@fandm.edu>
2023-07-28 17:31:06 -0400

    "Tomato tests done"

--- src/test/java/com/zipcodewilmington/froilansfarm/edibles/TomatoTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/edibles/TomatoTest.java
@@ -1,9 +1,28 @@
 package com.zipcodewilmington.froilansfarm.edibles;
 
+import org.junit.Assert;
+
 public class TomatoTest {
 
     public void InheritanceTest(){
         Tomato tomato = new Tomato();
+        Assert.assertTrue(tomato instanceof edible);
 
+    }
+
+    public void getTomatoTest(){
+        Tomato tomato = new Tomato();
+        boolean expected = true;
+        tomato.getHasBeenEaten = true;
+       boolean actual = tomato.getHasBeen();
+        Assert.assertEquals(expected, actual);
+    }
+
+    public void setTomatoTest(){
+        Tomato tomato = new Tomato();
+        boolean expected = true;
+        tomato.setHasBeenEaten(expected);
+        boolean actual = tomato.getHasBeenEaten();
+        Assert.assertEquals(expected, actual);
     }
 }
