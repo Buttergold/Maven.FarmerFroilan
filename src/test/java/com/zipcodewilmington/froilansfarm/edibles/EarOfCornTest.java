6c9f278c93457efad3448ad4eb13340881a73ca5
Danielle <deegreene2110@gmail.com>
2023-07-30 16:28:58 -0400

    ear of corn test done

--- src/test/java/com/zipcodewilmington/froilansfarm/edibles/EarOfCornTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/edibles/EarOfCornTest.java
@@ -1,4 +1,35 @@
 package com.zipcodewilmington.froilansfarm.edibles;
+import org.junit.Test;
+import org.junit.Assert;
 
 public class EarOfCornTest {
-}
+
+    @Test
+    public void testConstructor() {
+        EarOfCorn earOfCorn = new EarOfCorn();
+        
+        Assert.assertNotNull(earOfCorn);
+    }
+
+    @Test
+    public void testInheritance(){
+        EarOfCorn earOfCorn = new EarOfCorn();
+
+        Assert.assertTrue(earOfCorn instanceof Edible);
+    }
+
+    @Test
+    public void testGetAndSetEaten(){
+        boolean expected = true;
+        EarOfCorn earOfCorn = new EarOfCorn();
+
+        earOfCorn.setHasBeenEaten(expected);
+
+        boolean actual = earOfCorn.getHasBeenEaten();
+
+        Assert.assertEquals(expected, actual);
+
+    }
+    }
+
+
