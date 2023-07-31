780f0f5791f898319d97530725c1ad342657db4a
Christine <christine@ZipcodersMBP51.MG8702>
2023-07-31 00:03:23 -0400

    made sure the code is compiling

--- src/test/java/com/zipcodewilmington/froilansfarm/edibles/TomatoTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/edibles/TomatoTest.java
@@ -1,30 +1,35 @@
-//package com.zipcodewilmington.froilansfarm.edibles;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//
-//public class TomatoTest {
-//
-//    @Test
-//    public void InheritanceTest(){
-//        Tomato tomato = new Tomato();
-//        Assert.assertTrue(tomato instanceof Edible);
-//
-//    }
-//    @Test
-//    public void getTomatoTest(){
-//        Tomato tomato = new Tomato();
-//        boolean expected = true;
+package com.zipcodewilmington.froilansfarm.edibles;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TomatoTest {
+
+    @Test
+    public void InheritanceTest(){
+        Tomato tomato = new Tomato();
+        Assert.assertTrue(tomato instanceof Edible);
+
+    }
+    @Test
+    public void getTomatoTest(){
+        Tomato tomato = new Tomato();
+        boolean expected = true;
+        Assert.assertTrue(false);
+
+        // Instance Variables/Fields are usually PRIVATE and cannot be set directly
+        // please try using a getter or setter
+        
 //        tomato.getHasBeenEaten = true;
-//       boolean actual = tomato.getHasBeen();
+//        boolean actual = tomato.getHasBeen();
 //        Assert.assertEquals(expected, actual);
-//    }
-//    @Test
-//    public void setTomatoTest(){
-//        Tomato tomato = new Tomato();
-//        boolean expected = true;
-//        tomato.setHasBeenEaten(expected);
-//        boolean actual = tomato.getHasBeenEaten();
-//        Assert.assertEquals(expected, actual);
-//    }
-//}
+    }
+    @Test
+    public void setTomatoTest(){
+        Tomato tomato = new Tomato();
+        boolean expected = true;
+        tomato.setHasBeenEaten(expected);
+        boolean actual = tomato.getHasBeenEaten();
+        Assert.assertEquals(expected, actual);
+    }
+}
